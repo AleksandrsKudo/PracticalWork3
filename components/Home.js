f7453336204826fe059f968a855d0b59c3c9d532
AleksandrsKudo <95243279+AleksandrsKudo@users.noreply.github.com>
2021-12-30 17:25:51 +0200

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { Text, View, StyleSheet,Button } from 'react-native';
 import Constants from 'expo-constants';
 
@@ -39,3 +38,42 @@ const styles = StyleSheet.create({
   },
   
 });
+=======
+import { Text, View, StyleSheet, Button, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import { useState } from 'react';
+
+export default function HomeScreen({ navigation }) {
+  const [counter, setCounter] = useState(0);
+
+  return (
+
+    <View style={{ flex: 1, padding: 20 }}>
+
+      <Button
+        title='Go To ListView'
+        onPress={() => { navigation.navigate("List View") }}
+      />
+      <View style={{ marginTop: 10 }}></View>
+
+      <Button
+        title='Increase Counter'
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <View style={{ marginTop: 10 }}></View>
+      <Button
+        title='Decrease counter'
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text style={{ marginBottom: 20, }}> Counter value: {counter}</Text>
+
+    </View>
+
+
+
+  );
+}
